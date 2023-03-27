f963df49788d22a2e3d55029ff52c6da9b2b583c
sttephen <stephenatcollege@Gmail.com>
2023-03-27 01:20:29 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+
+
+</script>
+
+<svelte:head>
+    <title>MC Utils</title>
+
+</svelte:head>
+
+<h1>hey</h1>
---
+++ src/routes/[util]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+
+
+</script>
+
+<svelte:head>
+    <title>Skin Stealer | MC Utils</title>
+
+</svelte:head>
+
+<h1>hey</h1>
