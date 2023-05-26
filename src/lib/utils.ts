837f0e80dfbb48861c031051a9146a83d8ef0cea
devous <43449531+devous@users.noreply.github.com>
2023-05-27 00:49:09 +0100

    Add shortDescription for sidebar

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,30 +3,35 @@ import { existsSync, readFileSync, readdirSync } from "fs"
 const dir = "src/lib/component/util"
 const nameRegex = /const name = /
 const descriptionRegex = /const description = /
+const shortDescriptionRegex = /const shortDescription = /
 
 type Util = {
     path: string
     icon: string
     name: string
     description: string
+    shortDescription: string
 }
 
 export const utilList = async () => {
     const utils: Util[] = []
     readdirSync(`${dir}/component`).forEach((fileName: string) => {
         const path = fileName.replace(".svelte", "")
-        let name: string = ""
-        let description: string = ""
+        let name = ""
+        let description = ""
+        let shortDescription = ""
 
         readFileSync(`${dir}/component/${fileName}`, {encoding: 'utf8', flag: 'r'}).split(/\r?\n/).forEach((line: string) => {
             if (nameRegex.test(line)) name = clean(line, nameRegex)
             if (descriptionRegex.test(line)) description = clean(line, descriptionRegex)
+            if (shortDescriptionRegex.test(line)) shortDescription = clean(line, shortDescriptionRegex)
         })
         utils.push({
             path: path,
             icon: `/${dir}/icon/${path}.png`,
             name: name,
-            description: description
+            description: description,
+            shortDescription: shortDescription
         })
     })
     return utils
@@ -35,19 +40,22 @@ export const utilList = async () => {
 export const findUtilByPath = async (path: string) => {
     const componentUri = `${dir}/component/${path}.svelte`
     if (!existsSync(componentUri)) return undefined
-    let name: string = ""
-    let description: string = ""
+    let name = ""
+    let description = ""
+    let shortDescription = ""
     
     readFileSync(componentUri, {encoding: 'utf8', flag: 'r'}).split(/\r?\n/).forEach((line: string) => {
         if (nameRegex.test(line)) name = clean(line, nameRegex)
         if (descriptionRegex.test(line)) description = clean(line, descriptionRegex)
+        if (shortDescriptionRegex.test(line)) shortDescription = clean(line, shortDescriptionRegex)
     });
 
     return {
         path: path,
         icon: `/${dir}/icon/${path}.png`,
         name: name,
-        description: description
+        description: description,
+        shortDescription: shortDescription
     }
 }
 
