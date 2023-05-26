fe59ae1d389307c6dd1e6d49cea0bc17ad18514d
devous <43449531+devous@users.noreply.github.com>
2023-05-26 22:26:12 +0100

    complete profile api

---
+++ src/routes/api/profile/[username]/+server.ts
@@ -0,0 +1,45 @@
+import { json } from '@sveltejs/kit'
+import type { RequestHandler } from './$types'
+
+export const GET = (async ({ fetch, params }) => {
+
+	const request = await fetch(`https://api.ashcon.app/mojang/v2/user/${params.username}`)
+	const response = await request.json()
+
+    if (response.code) {
+        let message = "An unknown error occured."
+        if (response.code == 404) message = "The profile 't' was not found."
+        return json({
+            status: response.code,
+            message: message
+        })
+    }
+
+    const id = response.uuid.replaceAll("-", "")
+
+	return json({
+		uuid: response.uuid,
+        id: id,
+        username: response.username,
+        skin: {
+            png: {
+                url: response.textures.skin.url,
+                data: `data:image/png;base64,${response.textures.skin.data}`,
+                base64: response.textures.skin.data
+            },
+            texture: {
+                value: response.textures.raw.value,
+                signature: response.textures.raw.signature
+            },
+            extra: {
+                slim: response.textures.slim,
+                custom: response.textures.custom
+            }
+        },
+        renders: {
+            avatar: `https://crafatar.com/avatars/${id}`,
+            head: `https://crafatar.com/renders/head/${id}`,
+            body: `https://crafatar.com/renders/body/${id}`
+        }
+	})
+}) satisfies RequestHandler
