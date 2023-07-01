80e121d77462113e326f941b5c35d52081b723ac
stephendotgg <138625055+stephendotgg@users.noreply.github.com>
2023-07-01 06:16:12 +0100

    Util description fixes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,19 +8,19 @@ const utils: Util[] = [
     {
         path: "server-jars",
         name: "Server Jars",
-        description: "Download existing jar files for<br>popular server software.",
+        description: "Download existing jar files for<br> popular server software.",
         shortDescription: "Download server software jar"
     },
     {
         path: "start-file-generator",
         name: "Start File Generator",
-        description: "Generate server start file with<br>varying settings and flags.",
+        description: "Generate server start file with<br> varying settings and flags.",
         shortDescription: "Generate server jar start file"
     },
     {
         path: "note-block-songs",
         name: "Note Block Songs",
-        description: "Largest collection of Note Block Songs<br>(.nbs files) to use with <a href='https://www.spigotmc.org/resources/noteblockapi.19287/' target='_blank' class='underline decoration-dotted'>NoteblockAPI</a>.",
+        description: "Largest collection of Note Block Songs<br> (.nbs files) to use with <a href='https://www.spigotmc.org/resources/noteblockapi.19287/' target='_blank' class='underline decoration-dotted'>NoteblockAPI</a>.",
         shortDescription: "Largest .nbs file collection"
     },
     {
@@ -38,14 +38,14 @@ const utils: Util[] = [
     {
         path: "unicode-symbols",
         name: "Unicode Symbols",
-        description: "The full collection of all allowed<br>in-game unicode characters.",
+        description: "The full collection of all allowed<br> in-game unicode characters.",
         shortDescription: "All allowed unicode symbols"
     },
     {
         path: "gradient-generator",
         name: "Gradient Generator",
-        description: "Generate a gradient between two<br>colors for in-game text.",
-        shortDescription: "Create gradient hex codes"
+        description: "Generate a gradient between two<br> colors for in-game text.",
+        shortDescription: "Create chat color gradient"
     },
     {
         path: "server-info",
