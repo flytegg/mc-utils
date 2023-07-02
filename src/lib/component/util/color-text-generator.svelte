bc21f88a96576cd9bbc9c4e79d8d781d594c1e79
stephendotgg <138625055+stephendotgg@users.noreply.github.com>
2023-07-02 03:01:25 +0100

    Fix tab alignment issues on chat color gen

--- src/lib/component/util/color-text-generator.svelte
+++ src/lib/component/util/color-text-generator.svelte
@@ -159,7 +159,7 @@
     <div class="flex flex-col">
         <h3 class="font-medium text-white text-[20px] mt-8 text-center mb-5">Preview</h3>
         <div class="tab-system w-[100%] flex flex-col">
-            <div class="self-center grid lg:grid-cols-6 grid-cols-2">
+            <div class="self-center grid lg:grid-cols-5 grid-cols-2">
                 {#each tabData as tab, index}
                     <button class="button px-4 py-2 mr-2 rounded focus:outline-none"
                             class:bg-gray-200={index === activeTab}
