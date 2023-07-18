36ee6dcf2f55994c3cbd2974a0b9e936992d021b
stephendotgg <138625055+stephendotgg@users.noreply.github.com>
2023-07-18 23:15:32 +0100

    New buffed SEO page descriptions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,61 +3,71 @@ const utils: Util[] = [
         path: "server-jars",
         name: "Server Jars",
         description: "Download jar files for popular server software.",
-        shortDescription: "Download server software jar"
+        shortDescription: "Download server software jar",
+        seoDescription: "Download Minecraft server jars of any version including Vanilla, CraftBukkit, Spigot, Paper, Purpur, Folia, BungeeCord, Waterfall and Velocity."
     },
     {
         path: "item-ids",
         name: "Item IDs",
         description: "Search all in-game items and IDs.",
-        shortDescription: "Modern & legacy item IDs"
+        shortDescription: "Modern & legacy item IDs",
+        seoDescription: "An interactive list of all Minecraft item IDs, including current and legacy IDs (data values)."
     },
     {
         path: "start-file-generator",
         name: "Start File Generator",
         description: "Generate server start file with varying settings and flags.",
-        shortDescription: "Generate server jar start file"
+        shortDescription: "Generate server jar start file",
+        seoDescription: "A minimal script generator to produce start.bat or start.command files for Minecraft Spigot/Paper servers with customisable RAM and Flags (proxy & Aikar's flags)."
     },
     {
         path: "server-info",
         name: "Server Info",
         description: "Ping an address to get live server data.",
-        shortDescription: "Ping server for info"
+        shortDescription: "Ping server for info",
+        seoDescription: "A Minecraft server list like viewer to view live player counts, MOTD and favicon."
     },
     {
         path: "inventory-slots",
         name: "Inventory Slots",
         description: "Slot numbers for every inventory type.",
-        shortDescription: "Numbers for all inventories"
+        shortDescription: "Numbers for all inventories",
+        seoDescription: "A list of Minecraft Bukkit inventory slot IDs including chests, double chests, furnace, player, villager, brewing stand, crafting table and many more."
     },
     {
         path: "skin-stealer",
         name: "Skin Stealer",
         description: "Steal a player's skin from their IGN.",
-        shortDescription: "Steal another player's skin"
+        shortDescription: "Steal another player's skin",
+        seoDescription: "View and download any Minecraft player's skin by searching their in-game name."
     },
     {
         path: "color-codes",
         name: "Color Codes",
         description: "List of built in color and format codes.",
-        shortDescription: "All default color codes"
+        shortDescription: "All default color codes",
+        seoDescription: "Table of Minecraft color codes along with the chat code (&), the MiniMessage tag, the MOTD code and the hex codes."
     },
     {
         path: "color-text-generator",
         name: "Color Text Generator",
         description: "Generate and preview text with color codes.",
-        shortDescription: "Create text with color codes"
+        shortDescription: "Create text with color codes",
+        seoDescription: "Generate text using Minecraft color codes & custom hex codes and preview as text, in sign, a book, chat and MOTD."
     },
     {
         path: "gradient-generator",
         name: "Gradient Generator",
         description: "Create a gradient between two colors for in-game text.",
-        shortDescription: "Create chat color gradient"
+        shortDescription: "Create chat color gradient",
+        seoDescription: "Create a custom gradient between two colors and receive Spigot ChatColor and MiniMessage output for use in Minecraft plugins."
     },
     {
         path: "small-text-converter",
         name: "Small Text Converter",
         description: "Convert into sᴍᴀʟʟ ᴛᴇxᴛ seen on new era servers.",
-        shortDescription: "Convert into sᴍᴀʟʟ ᴛᴇxᴛ"
+        shortDescription: "Convert into sᴍᴀʟʟ ᴛᴇxᴛ",
+        seoDescription: "Convert text into small caps font seen on Minecraft servers like MCCI- uppercase-like, yet tiny, small capital letters."
     },
     // {
     //     path: "unicode-symbols",
@@ -69,19 +79,22 @@ const utils: Util[] = [
         path: "note-block-songs",
         name: "Note Block Songs",
         description: "Largest collection of .nbs files to use with <a href='https://www.spigotmc.org/resources/noteblockapi.19287/' aria-label='NoteblockAPI' target='_blank' class='underline decoration-dotted'>NoteblockAPI</a>.",
-        shortDescription: "Largest .nbs file collection"
+        shortDescription: "Largest .nbs file collection",
+        seoDescription: "The largest collection of Noteblock Studio songs (.nbs files) for use in NoteBlockAPI to play Note Block music in Minecraft."
     },
     {
         path: "banner-creator",
         name: "Banner Creator",
         description: "Create a banner design and get in-game commands and code.",
-        shortDescription: "Craft banners and get code"
+        shortDescription: "Craft banners and get code",
+        seoDescription: "Create Minecraft banners using a range of colors or patterns and receive the /give command and Spigot API code."
     },
     {
         path: "daylight-cycle",
         name: "Daylight Cycle",
         description: "Convert real time into Minecraft time.",
-        shortDescription: "Convert real time to MC time"
+        shortDescription: "Convert real time to MC time",
+        seoDescription: "Convert real life time into Minecraft ticks and view a daylight time graph. "
     }
 ]
 
@@ -90,6 +103,7 @@ type Util = {
     name: string
     description: string
     shortDescription: string
+    seoDescription: string
 }
 
 export const utilList = async () => {
