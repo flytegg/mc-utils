d6dc9970b24a17c0f54c11962dcc2c448272c07b
Stephen <138153996+stephendotgg@users.noreply.github.com>
2023-07-20 08:56:56 +0100

    syntx

--- src/routes/[util]/webhook.ts
+++ src/routes/[util]/webhook.ts
@@ -1,5 +1,5 @@
 export async function sendToDiscordWebhook(messageContent: string) {
-    let webhookURL = process.env['VITE_DISCORD_WEBHOOK']
+    let webhookURL = process.env.VITE_DISCORD_WEBHOOK
     if (!webhookURL) { return }
 
     await fetch(webhookURL, {
