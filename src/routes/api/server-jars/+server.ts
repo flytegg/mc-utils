8ed29669b6e0667b6ce02b8b1fb581304ffafba1
Josh <jshbker@gmail.com>
2023-11-04 21:32:42 +0000

    change platform to have a key and a display

--- src/routes/api/server-jars/+server.ts
+++ src/routes/api/server-jars/+server.ts
@@ -3,5 +3,10 @@ import type { RequestHandler } from './$types'
 import info from "$lib/server-jars.json"
 
 export const GET = (async () => {
-	return json(info.map((item) => item.platform))
+	return json(info.map((item) => {
+		return { 
+			key: item.platform,
+			name: item.display
+		}
+	}))
 }) satisfies RequestHandler
