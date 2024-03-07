21b5c9c32201da81425a296cb531e31863b02499
Josh <43449531+joshbker@users.noreply.github.com>
2024-03-07 18:14:10 +0000

    feat: so many lines

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,7 @@
 import type {LayoutLoad} from './$types'
 
 export const load: LayoutLoad = (async ({fetch}) => {
-    const response = await fetch(`/api/util`)
-    const json = await response.json()
     return {
-        utils: json.content
+        utils: (await (await fetch(`/api/util`)).json()).content
     }
 })
