6de10464a1b5be461fa59d8febc71a0c5698d3a7
Stephen <138625055+stephendotgg@users.noreply.github.com>
2024-03-07 20:50:52 +0000

    All tables done

--- src/lib/component/util/item-ids.svelte
+++ src/lib/component/util/item-ids.svelte
@@ -31,30 +31,30 @@
     <img src="/icon/search.svg" alt="Search Icon" class="h-5">
     <input on:keypress={handleKeyPress} on:blur={handleInput} bind:value={searchValue} on:input={handleInput} type="text" placeholder="Enter item name..." class="w-full pt-0.5 pr-0.5 justify-center items-center self-center">
 </div>
-<table class="w-[90%] md:w-[80%] lg:w-[60%] text-white mt-12 table-fixed">
-    <tr class="bg-[#1d1f24]">
-        <th class="rounded-tl-lg rounded-bl-lg p-2 pl-6 w-0 pr-2"></th>
-        <th class="font-medium text-[20px] text-left p-2 pr-4">Display Name</th>
-        <th class="font-medium text-[20px] text-left pr-4">ID (1.13+)</th>
-        <th class="font-medium text-[20px] text-left pr-4 w-[24%]">Legacy ID (1.12-)</th>
-        <th class="rounded-tr-lg rounded-br-lg font-medium text-[20px] text-left pr-3 w-[15%]">Stack Size</th>
+<table class="w-[90%] md:w-[80%] lg:w-[60%] mt-12 table-fixed">
+    <tr class="text-[#9d9d9e] font-medium">
+        <th class="p-2 pl-6 w-0 pr-2"></th>
+        <td class="p-2 pr-4 border-b-[1.5px] border-b-[#232324]">Display Name</td>
+        <td class="text-left pr-4 border-b-[1.5px] border-b-[#232324]">ID (1.13+)</td>
+        <td class="text-left pr-4 w-[24%] border-b-[1.5px] border-b-[#232324]">Legacy ID (1.12-)</td>
+        <td class="text-left pr-3 w-[15%] border-b-[1.5px] border-b-[#232324]">Stack Size</td>
     </tr>
 
     {#each searchResults as item}
-        <tr class="">
-            <td class="lg:ml-6 p-1.5">
+        <tr class="text-[#cecece]">
+            <td class="lg:ml-6 p-1.5 ">
                 {#if item.texture}
                     <img src={item.texture} alt="{item.name} Icon" class="h-5 w-5">
                 {/if}
             </td>
-            <td class="p-1.5 border-b-2 border-b-[#1D1F24] text-gray-400">{item.name}</td>
-            <td class="border-b-2 border-b-[#1D1F24] text-gray-400 break-all">{item.newID}</td>
-            <td class="border-b-2 border-b-[#1D1F24] text-gray-400">
+            <td class="p-2 border-b-[1px] border-b-[#232324]">{item.name}</td>
+            <td class="break-all border-b-[1px] border-b-[#232324]">{item.newID}</td>
+            <td class="border-b-[1px] border-b-[#232324]">
                 {#if item.legacyID}
                     {item.legacyID}
                 {/if}
             </td>
-            <td class="border-b-2 border-b-[#1D1F24] text-gray-400">{item.stackSize}</td>
+            <td class="border-b-[1px] border-b-[#232324]">{item.stackSize}</td>
         </tr>
     {/each}
 </table>
