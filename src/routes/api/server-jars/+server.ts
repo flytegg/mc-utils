109869fa481c3dee7d46319fe7c72f928c2a0cbc
Josh <43449531+joshbker@users.noreply.github.com>
2024-03-29 01:52:11 +0000

    feat: live server jars api

--- src/routes/api/server-jars/+server.ts
+++ src/routes/api/server-jars/+server.ts
@@ -1,10 +1,5 @@
 import {json} from '@sveltejs/kit'
 import type {RequestHandler} from './$types'
-import info from "$lib/server-jars/server-jars.json"
+import { fetchSupportedPlatforms } from '$lib/server-jars/server-jar-utils'
 
-export const GET: RequestHandler = (async () => json(info.map((item) => {
-    return {
-        key: item.platform,
-        name: item.display
-    }
-})))
+export const GET: RequestHandler = async () => json(fetchSupportedPlatforms())
