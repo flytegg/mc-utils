df0d33368851468746095b90b53960e7f5c03ed3
Stephen <138625055+stephendotgg@users.noreply.github.com>
2024-04-12 23:41:02 +0100

    small text fixes

--- src/lib/component/util/small-text-converter.svelte
+++ src/lib/component/util/small-text-converter.svelte
@@ -45,12 +45,12 @@
 <main class="w-[60%] grid grid-cols-1 md:grid-cols-2 gap-10 text-center text-white">
     <div class="flex flex-col gap-3">
         <h3 class="font-medium text-white text-[20px]">Input</h3>
-        <textarea class="w-full text-lg text-gray-400 font-mono rounded-md p-2 bg-[#141517] resize-none h-auto min-h-[400px]" bind:value={inputText} on:input />
+        <textarea class="w-full text-lg text-gray-400 font-mono rounded-md p-2 bg-[#141517] resize-none h-auto min-h-[400px] leading-6" bind:value={inputText} on:input />
     </div>
     <div class="flex flex-col gap-3">
-        <h3 class="font-medium text-white text-[20px]">sᴍᴀʟʟ ᴛᴇxᴛ</h3>
+        <h3 class="font-medium text-white text-[20px]" style="font-family: sans-serif">sᴍᴀʟʟ ᴛᴇxᴛ</h3>
         <div class="relative">
-            <textarea class="w-full text-lg text-gray-400 rounded-md p-2 bg-[#141517] resize-none h-auto min-h-[400px]" disabled on:copy={copyToast}>{convertedText}</textarea>
+            <textarea class="w-full text-lg text-gray-400 rounded-md p-2 bg-[#141517] resize-none h-auto min-h-[400px] leading-6" style="font-family: sans-serif" disabled on:copy={copyToast}>{convertedText}</textarea>
             <button class="text-sm px-4 py-1.5 absolute right-5 bottom-6 button bg-[#141517]" on:click={copyValue}>Copy</button>
         </div>
     </div>
