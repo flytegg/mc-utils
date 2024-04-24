94f7f0f69bede9e21b8a7aedca9f7565dd138e7c
Stephen <138625055+stephendotgg@users.noreply.github.com>
2024-04-24 02:13:14 +0100

    cf fix

--- src/routes/api/tracker/[stat]/+server.ts
+++ src/routes/api/tracker/[stat]/+server.ts
@@ -1,6 +1,6 @@
 import type {RequestHandler} from "./$types"
 import {error, json} from "@sveltejs/kit";
-import {trackerCollection} from "$lib/tracker/mongo";
+// import {trackerCollection} from "$lib/tracker/mongo";
 
 const allowedStats = ["server-jars-served", "start-files-served", "server-infos-served", "skins-searched", "skins-served", "capes-searched", "capes-served", "color-text-copied", "gradients-copied", "small-text-copied", "server-icons-served", "note-block-songs-served", "banners-served", "fireworks-served"]
 
@@ -10,7 +10,7 @@ export const POST: RequestHandler = (async ({params}) => {
         throw error(400, "Invalid statistic")
     }
 
-    await trackerCollection.updateOne({}, {$inc: {[stat]: 1}})
+    // await trackerCollection.updateOne({}, {$inc: {[stat]: 1}})
 
     return json({
         status: 200,
