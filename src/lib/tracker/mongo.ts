ffd05413a049e431bceb899e553a7a5ff65238d6
Stephen <138625055+stephendotgg@users.noreply.github.com>
2024-04-24 01:26:33 +0100

    please

--- src/lib/tracker/mongo.ts
+++ src/lib/tracker/mongo.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose"
+import {platform} from "os";
 
 // @ts-ignore
-mongoose.connect(process.env.MONGO_TRACKING_URI)
+mongoose.connect(platform.env.MONGO_TRACKING_URI)
 export const trackerCollection = mongoose.connection.useDb("mc-utils").collection("tracker")
