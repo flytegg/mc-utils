64733cd89c56438063447bbde90a5e603b98dfa8
Stephen <138625055+stephendotgg@users.noreply.github.com>
2024-04-24 01:45:09 +0100

    cf fix

--- src/lib/tracker/mongo.ts
+++ src/lib/tracker/mongo.ts
@@ -1,6 +1,6 @@
-import mongoose, {connect} from 'mongoose';
-
 import {MONGO_TRACKING_URI} from "$env/static/private";
 
+const mongoose = require('mongoose');
 mongoose.connect(MONGO_TRACKING_URI)
+
 export const trackerCollection = mongoose.connection.useDb("mc-utils").collection("tracker")
